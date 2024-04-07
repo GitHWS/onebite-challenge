aa8402f9881a4101fe2578c2561566c9d2c90cc1
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2024-04-07 23:16:16 +0900

    add day13 assignment

--- onebite-react/day13/src/App.jsx
+++ onebite-react/day13/src/App.jsx
@@ -1,7 +1,55 @@
 import './App.css';
+import { useState } from 'react';
+
+function CurrenyInput({ curreny, value, onChange }) {
+  return (
+    <div>
+      <label>{curreny} : </label>
+      <input
+        type="number"
+        value={value}
+        onChange={(e) => onChange(curreny, e.target.value)}
+      />
+    </div>
+  );
+}
 
 function App() {
-  return <></>;
+  const exchangeRate = 1353;
+  const [currenyRate, setCurrenyRate] = useState({
+    krw: 0,
+    usd: 0,
+  });
+
+  const onChangeCurrenyRate = (curreny, value) => {
+    if (curreny === 'KRW') {
+      setCurrenyRate({
+        krw: value,
+        usd: value / exchangeRate,
+      });
+    } else {
+      setCurrenyRate({
+        krw: value * exchangeRate,
+        usd: value,
+      });
+    }
+  };
+
+  return (
+    <>
+      <h1>환율 변환기(KRW-USD)</h1>
+      <CurrenyInput
+        curreny="KRW"
+        value={currenyRate.krw}
+        onChange={onChangeCurrenyRate}
+      />
+      <CurrenyInput
+        curreny="USD"
+        value={currenyRate.usd}
+        onChange={onChangeCurrenyRate}
+      />
+    </>
+  );
 }
 
 export default App;
