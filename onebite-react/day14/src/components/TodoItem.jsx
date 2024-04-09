664c39c0c532986f84ad4ca8ca76811e82217e43
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2024-04-09 16:51:11 +0900

    add day14 finish file

---
+++ onebite-react/day14/src/components/TodoItem.jsx
@@ -0,0 +1,30 @@
+import './TodoItem.css';
+
+const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
+  const onClickDeleteButton = () => {
+    const isConfirmed = confirm('정말로 삭제하시겠습니까?');
+    if (isConfirmed) {
+      onDelete(id);
+    }
+  };
+
+  return (
+    <div className="TodoItem">
+      <input
+        onChange={onChangeCheckbox}
+        readOnly
+        checked={isDone}
+        type="checkbox"
+      />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button onClick={onClickDeleteButton}>삭제</button>
+    </div>
+  );
+};
+
+export default TodoItem;
