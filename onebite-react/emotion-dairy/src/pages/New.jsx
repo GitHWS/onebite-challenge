d949126e80f7b8eadb600364e41203515731a670
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2024-04-17 23:18:01 +0900

    add UI and feature in edit page

--- onebite-react/emotion-dairy/src/pages/New.jsx
+++ onebite-react/emotion-dairy/src/pages/New.jsx
@@ -1,5 +1,29 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { DiaryDispatchContext } from '../App';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Editor from '../components/Editor';
+
 const New = () => {
-  return <div>New</div>;
+  const nav = useNavigate();
+  const { onCreate } = useContext(DiaryDispatchContext);
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    // replace 옵션 : 뒤로가기 방지
+    nav('/', { replace: true });
+  };
+
+  return (
+    <div>
+      <Header
+        title="새 일기 쓰기"
+        leftChild={<Button text="< 뒤로가기" onClick={() => nav(-1)} />}
+      />
+      <Editor onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default New;
