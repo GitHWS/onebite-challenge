6066faefb8e61cc6bd7e600703c1acc42dcac95b
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2024-04-18 10:59:22 +0900

    add UI and feature in Diary, Edit page

--- onebite-react/emotion-dairy/src/pages/Diary.jsx
+++ onebite-react/emotion-dairy/src/pages/Diary.jsx
@@ -1,10 +1,36 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { getStringedDate } from '../util/get-stringed-date';
+import useDiary from '../hooks/useDiary';
+
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Viewer from '../components/Viewer';
 
 const Diary = () => {
   const { id } = useParams();
-  console.log(id);
+  const nav = useNavigate();
 
-  return <div>{id}번 일기입니다.</div>;
+  const curDiaryItem = useDiary(id);
+
+  if (!curDiaryItem) {
+    return <div>데이터 로딩 중...</div>;
+  }
+
+  const { createdDate, emotionId, content } = curDiaryItem;
+  const title = getStringedDate(new Date(createdDate));
+
+  return (
+    <div>
+      <Header
+        title={`${title} 기록`}
+        leftChild={<Button onClick={() => nav(-1)} text="< 뒤로 가기" />}
+        rightChild={
+          <Button onClick={() => nav(`/edit/${id}`)} text="수정하기" />
+        }
+      />
+      <Viewer emotionId={emotionId} content={content} />
+    </div>
+  );
 };
 
 export default Diary;
--- onebite-react/emotion-dairy/src/pages/Edit.jsx
+++ onebite-react/emotion-dairy/src/pages/Edit.jsx
@@ -1,8 +1,44 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useContext } from 'react';
+import { DiaryDispatchContext } from '../App';
+import useDiary from '../hooks/useDiary';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Editor from '../components/Editor';
+
 const Edit = () => {
   const { id } = useParams();
+  const nav = useNavigate();
+  const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
 
-  return <div>{id}번 일기 수정 페이지입니다</div>;
+  const curDiaryItem = useDiary(id);
+
+  const onClickDelete = () => {
+    if (confirm('일기를 정말 삭제할까요? 다시 복구되지 않아요!')) {
+      onDelete(id);
+      nav('/', { replace: true });
+    }
+  };
+
+  const onSubmit = (input) => {
+    if (confirm('일기를 정말 수정할까요?')) {
+      onUpdate(id, input.createdDate.getTime(), input.emotionId, input.content);
+      nav('/', { replace: true });
+    }
+  };
+
+  return (
+    <div>
+      <Header
+        title="일기 수정하기"
+        leftChild={<Button text="< 뒤로 가기" onClick={() => nav(-1)} />}
+        rightChild={
+          <Button text="삭제하기" type="NEGATIVE" onClick={onClickDelete} />
+        }
+      />
+      <Editor initData={curDiaryItem} onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default Edit;
